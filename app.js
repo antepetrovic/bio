3947841ec36c6864c76f7b7bff4139e1bc659f9b
ante <ante@gmail.com>
2017-11-07 10:27:46 +0100

    mobile near end

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 $(document).ready(function(){
-    $('header div').click(function(){
+    $("#nav-icon").click(function(){
         $(this).toggleClass('open');
         $( "nav" ).slideToggle("slow");
     });
 
+    $(window).scroll(function(){
+        if ($(this).scrollTop() > 100) {
+            $('.scrollToTop').fadeIn();
+        } else {
+            $('.scrollToTop').fadeOut();
+        }
+    });
 
+    //Click event to scroll to top
+    $('.scrollToTop').click(function(){
+        $('html, body').animate({scrollTop : 0},1000);
+        return false;
+    });
 });
+
