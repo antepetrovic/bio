ffda9d6705d6488c6076439cec5f5ae2839bc55f
ante <ante@gmail.com>
2017-11-08 16:51:20 +0100

    added fade in

--- app.js
+++ app.js
@@ -17,5 +17,38 @@ $(document).ready(function(){
         $('html, body').animate({scrollTop : 0},1000);
         return false;
     });
+
+    $(function () {
+        var element = $("#second");
+        $(window).scroll(function () {
+            if($(window).scrollTop() > 200) {
+                element.addClass("animateMe");
+            }
+
+        });
+    });
+
+    $(function () {
+        var element = $("#third");
+        $(window).scroll(function () {
+            if($(window).scrollTop() > 700) {
+                element.addClass("animateMe");
+            }
+
+        });
+    });
+
+    $(function () {
+        var element = $("#fourth");
+        $(window).scroll(function () {
+            if($(window).scrollTop() > 1400) {
+                element.addClass("animateMe");
+            }
+
+        });
+    });
+
 });
+var a = $("#fourth").position();
+console.log(a);
 
