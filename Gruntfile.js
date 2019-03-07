310470df28ce2aaf60e68c64e2abc6077ec9b558
Ante Petrovic <lelomacbook@gmail.com>
2019-03-07 18:35:00 +0100

    Modified

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,48 +2,63 @@ module.exports = function(grunt) {
 
   // Project configuration.
   grunt.initConfig({
+
+    sass: {
+      dist: {
+        options: {
+          style: 'expanded'
+        },
+        files: {
+          'scss/main.css': 'scss/main.scss',
+        }
+      }
+    },
     
     cssmin: {
       target: {
-           src: ['css/main.css'],
+           src: ['scss/main.css'],
           dest: 'dist/main.min.css',
           ext: '.min.css'
       }
     },
+
     uglify: {
       options: {
       mangle: false
-      },
-      my_target: {
-        files: {
-          'dist/app.min.js': ['dist/built.js']
-          }
+    },
+    
+    my_target: {
+      files: {
+        'dist/app.min.js': ['dist/built.js']
+        }
+      }
+    },
+
+    concat: {
+        options: {
+            separator: ';'
+        },
+        dist: {
+            src: ['bioenergija.js', 'app.js'],
+            dest: 'dist/built.js'
         }
     },
-      watch: {
-          css: {
-              files: ['css/main.css', 'app.js'],
-              tasks: ['cssmin','concat', 'uglify']
-          }
-      },
-      concat: {
-          options: {
-              separator: ';'
-          },
-          dist: {
-              src: ['bioenergija.js', 'app.js'],
-              dest: 'dist/built.js'
-          }
+
+    watch: {
+      css: {
+          files: ['scss/*.scss', 'app.js'],
+          tasks: ['sass', 'cssmin', 'concat', 'uglify']
       }
+    },
   });
 
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-cssmin');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-sass');
 
-  grunt.registerTask('build', ['cssmin', 'concat','uglify']);
-  grunt.registerTask('default', ['cssmin', 'concat','uglify','watch']);
+  grunt.registerTask('default', 'watch');
 };
 
 
